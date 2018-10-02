79e11301526849dad3a20343543599d91c25a03a
LeeVaughn <dlvprogramming@gmail.com>
2018-10-02 11:21:38 -0700

    updated to arrow functions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,37 +4,37 @@ const express = require("express");
 const router = express.Router();
 
 // GET /api/users
-router.get("/users", function(req, res) {
+router.get("/users", (req, res) => {
   res.json({ response: "You sent me a GET request" });
 });
 
 // POST /api/users
-router.post("/users", function(req, res) {
+router.post("/users", (req, res) => {
   res.json({ response: "You sent me a POST request", body: req.body });
 });
 
 // GET /api/courses
-router.get("/courses", function(req, res) {
+router.get("/courses", (req, res) => {
   res.json({ response: "You sent me a GET request" });
 });
 
 // POST /api/courses
-router.post("/courses", function(req, res) {
+router.post("/courses", (req, res) => {
   res.json({ response: "You sent me a POST request", body: req.body });
 });
 
 // GET /api/courses/:courseId
-router.get("/courses/:id", function(req, res) {
+router.get("/courses/:id", (req, res) => {
   res.json({ response: "You sent me a GET request for ID " + req.params.id });
 });
 
 // PUT /api/courses/:courseId
-router.put("/courses/:id", function(req, res) {
+router.put("/courses/:id", (req, res) => {
   res.json({ response: "You sent me a PUT request for ID " + req.params.id });
 });
 
 // POST /api/courses/:courseId
-router.post("/courses/:id/reviews", function(req, res) {
+router.post("/courses/:id/reviews", (req, res) => {
   res.json({ response: "You sent me a POST request for ID " + req.params.id });
 });
 
