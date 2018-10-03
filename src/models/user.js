e3b1d9c7e70dc4b036738830819faad5f9b62531
LeeVaughn <dlvprogramming@gmail.com>
2018-10-02 20:07:21 -0700

    added authorization and password hashing functionality

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
   fullName: {
@@ -19,6 +20,43 @@ const UserSchema = new Schema({
     type: String,
     required: true
   }
+});
+
+// authenticate input against database documents
+UserSchema.statics.authenticate = (email, password, callback) => {
+  User.findOne({ emailAddress: email })
+    .exec((err, user) => {
+      if (err) {
+        return callback(err);
+      } else if ( !user ) {
+        const err = new Error("User not found");
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, (error, result) => {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          const err = new Error("Authentication failed");
+          err.status = 401;
+          return callback(err);
+        }
+      });
+    });
+}
+
+// hashes password before saving to database
+UserSchema.pre("save", function(next) {
+  const user = this;
+
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      next(err);
+    }
+    // replaces plain text password with hashed password
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model("User", UserSchema);
