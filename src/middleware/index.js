e1edf6c62da7b7862fc9d527230c8cac5115fc77
LeeVaughn <dlvprogramming@gmail.com>
2018-10-02 20:48:24 -0700

    created middleware for authentifiction

---
+++ src/middleware/index.js
@@ -0,0 +1,24 @@
+const User = require("../models/user");
+const auth = require("basic-auth");
+
+function authenticateUser(req, res, next) {
+  const credentials = auth(req);
+
+  if (credentials) {
+    User.authenticate(credentials.name, credentials.pass, (error, user) => {
+      if (error) {
+        const err = new Error("Unable to authenticate credentials");
+        err.status = 401;
+        next(err);
+      }
+      req.user = user;
+      next();
+    });
+  } else (
+    const err = new Error("Please enter your credentials");
+    err.status = 401;
+    next(err);
+  )
+}
+
+module.exports.authenticateUser = authenticateUser;
